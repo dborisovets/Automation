61dbfd435d63a8a3039220549c6bba33978bbbf5
Darya.Barysavets <Darya.Barysavets@NBA-059-V1-LT>
2022-10-24 17:52:36 +0300

    Add project files.

---
+++ dasha aqa/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! What's your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"{Environment.NewLine}Nice to meet you, {name}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
